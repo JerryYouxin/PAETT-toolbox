1f26d7d54daa5f80f38c47f080e6871c7c7107b5
JerryYouxin <youxin2015@outlook.com>
2020-12-24 10:07:41 +0800

    fix path typo

--- PAETT-tool/benchmarks/XSBench/collect.sh
+++ PAETT-tool/benchmarks/XSBench/collect.sh
@@ -9,7 +9,7 @@ pushd src
 module load clang
 
 echo -e ${GREEN}Collecting data for ${RED}XSBench${NC}
-mkdir -p ../COLLECT/
+mkdir -p ../../COLLECT/XSBench
 chmod +x run-inst.sh
 collect_data --keymap=detect/PAETT.keymap --out=../../COLLECT/XSBench/metric.out --exe=./run-inst.sh --papi=PAPI_BR_NTK,PAPI_LD_INS,PAPI_L2_ICR,PAPI_BR_MSP,PAPI_RES_STL,PAPI_SR_INS,PAPI_L2_DCR --consistant
 
